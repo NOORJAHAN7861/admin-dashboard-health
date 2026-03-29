0c94d360ffc0b2cc4eaf9a2567fddb004131b464
unknown <noorjahanpp786@gmail.com>
2026-03-29 20:53:00 +0530

    port changed

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
+
 export const api = axios.create({
-  baseURL: "https://healthapp-backend-production-a7de.up.railway.app",
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5100",
   withCredentials: true,
 });
